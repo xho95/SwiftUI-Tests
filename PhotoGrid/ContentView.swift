da3ff3e73083eed0b4a9582af5135f0a21f66e2d
Min-ho Kim <xho1995@gmail.com>
2020-11-05 15:50:56 +0900

    Initial Commit

---
+++ PhotoGrid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhotoGrid
+//
+//  Created by Kim, Min Ho on 2020/11/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhotoGrid/PhotoGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoGridApp.swift
+//  PhotoGrid
+//
+//  Created by Kim, Min Ho on 2020/11/05.
+//
+
+import SwiftUI
+
+@main
+struct PhotoGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
