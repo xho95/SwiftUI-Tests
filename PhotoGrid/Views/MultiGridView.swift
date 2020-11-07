9bf55684320dc5033c3b622d1650fa21d08c3aca
Min-ho Kim <xho1995@gmail.com>
2020-11-07 19:34:20 +0900

    fixes the app for the iPad

--- PhotoGrid/Views/MultiGridView.swift
+++ PhotoGrid/Views/MultiGridView.swift
@@ -8,29 +8,46 @@
 import SwiftUI
 
 struct MultiGridView: View {
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass: UserInterfaceSizeClass?
+    @Environment(\.verticalSizeClass) var verticalSizeClass: UserInterfaceSizeClass?
+    
     @State var gridLayout = [GridItem()]
     
     @ObservedObject var viewModel: PhotoLibrary
 
     var body: some View {
-        NavigationView {
-            ScrollView {
-                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
-                    ForEach(viewModel.cafes) { cafe in
-                        Image(cafe.image)
-                            .resizable()
-                            .scaledToFill()
-                            .frame(minWidth: 0, maxWidth: .infinity)
-                            .frame(maxHeight: 150)
-                            .cornerRadius(10)
-                            .shadow(color: Color.primary.opacity(0.3), radius: 1)
-                        InnerGridView(cafe: cafe)
+        GeometryReader { geometry in
+            NavigationView {
+                ScrollView {
+                    LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                        ForEach(viewModel.cafes) { cafe in
+                            Image(cafe.image)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(minWidth: 0, maxWidth: .infinity)
+                                .frame(maxHeight: 150)
+                                .cornerRadius(10)
+                                .shadow(color: Color.primary.opacity(0.3), radius: 1)
+                            InnerGridView(cafe: cafe)
+                        }
+                    }
+                    .padding(.all, 10)
+                    .animation(.interactiveSpring())
+                }
+                .onAppear {
+                    switch UIDevice.current.userInterfaceIdiom {
+                    case .pad:
+                        gridLayout = [GridItem(), GridItem()]
+                    default:
+                        break
                     }
                 }
-                .padding(.all, 10)
-                .animation(.interactiveSpring())
+                .navigationTitle("Coffee Feed")
+                .onChange(of: verticalSizeClass) { value in
+                    gridLayout = [GridItem(.adaptive(minimum: verticalSizeClass == .compact ? 100 : 250 )), GridItem(.flexible())]
+                }
             }
-            .navigationTitle("Coffee Feed")
+            .navigationViewStyle(StackNavigationViewStyle())
         }
     }
 }
