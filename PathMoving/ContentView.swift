280ab043d38acf7109a517a30c074c5358c8c90c
Min-ho Kim <xho1995@gmail.com>
2021-02-20 13:51:05 +0900

    Initial Commit

---
+++ PathMoving/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PathMoving
+//
+//  Created by Kim, Min Ho on 2021/02/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PathMoving/PathMovingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathMovingApp.swift
+//  PathMoving
+//
+//  Created by Kim, Min Ho on 2021/02/20.
+//
+
+import SwiftUI
+
+@main
+struct PathMovingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
