cdbcca1bf24d55f9cb02018eefcfdf13863a0322
Kim, Min Ho <xho1995@gmail.com>
2021-04-01 20:42:18 +0900

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Kim, Min Ho on 2021/04/01.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
