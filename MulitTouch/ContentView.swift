8b715b9cda90752b7509d34a29d9f3e06f6b64b9
Kim, Min Ho <xho1995@gmail.com>
2021-04-11 14:59:24 +0900

    Initial Commit

---
+++ MulitTouch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MulitTouch
+//
+//  Created by Kim, Min Ho on 2021/04/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MulitTouch/MulitTouchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MulitTouchApp.swift
+//  MulitTouch
+//
+//  Created by Kim, Min Ho on 2021/04/11.
+//
+
+import SwiftUI
+
+@main
+struct MulitTouchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
