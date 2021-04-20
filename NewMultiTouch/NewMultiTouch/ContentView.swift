deccb48282ee98bbd841cdee1afc6bc1a90cc7f6
Kim, Min Ho <xho1995@gmail.com>
2021-04-20 19:14:57 +0900

    starts the New Multi Touch project

---
+++ NewMultiTouch/NewMultiTouch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewMultiTouch
+//
+//  Created by Kim, Min Ho on 2021/04/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewMultiTouch/NewMultiTouch/NewMultiTouchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewMultiTouchApp.swift
+//  NewMultiTouch
+//
+//  Created by Kim, Min Ho on 2021/04/20.
+//
+
+import SwiftUI
+
+@main
+struct NewMultiTouchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
