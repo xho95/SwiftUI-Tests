58416cf5807d1d121bbf9fa923863959a9220f2b
Kim, Min Ho <xho1995@gmail.com>
2021-04-20 16:20:54 +0900

    adds the Timer codes

--- Timer/ContentView.swift
+++ Timer/ContentView.swift
@@ -8,9 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    let timer = Timer
+        .publish(every: 1, on: .main, in: .common)
+        .autoconnect()
+    /*
+    let timer = Timer
+        .publish(every: 1, tolerance: 0.5, on: .main, in: .common)
+        .autoconnect()
+     */
+    @State private var counter = 0
+    @State private var date = Date()
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello, world!")
+                .padding()
+            Text("\(date)")
+                .onReceive(timer) { time in
+                    if self.counter == 10 {
+                        self.timer.upstream.connect().cancel()
+                    } else {
+                        print("The time is now \(time)")
+                    }
+                    
+                    self.counter += 1
+                    self.date = time
+                }
+                .padding()
+            /*
+            Text("\(Calendar.current.dateComponents([.hour, .minute, .second], from: date))")
+                .onReceive(timer) { time in
+                    if self.counter == 10 {
+                        self.timer.upstream.connect().cancel()
+                    } else {
+                        print("The time is now \(time)")
+                    }
+                    
+                    self.counter += 1
+                    self.date = time
+                }
+                .padding()
+             */
+        }
     }
 }
 
