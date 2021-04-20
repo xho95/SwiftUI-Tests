60f353ffad2dfdf50c68b642d831abbcfa80d31a
Kim, Min Ho <xho1995@gmail.com>
2021-04-20 19:27:30 +0900

    adds the source files from the broken MultiTouch app

---
+++ NewMultiTouch/NewMultiTouch/DeviceRotation.swift
@@ -0,0 +1,28 @@
+//
+//  DeviceRotation.swift
+//  MulitTouch
+//
+//  Created by Kim, Min Ho on 2021/04/12.
+//
+
+import SwiftUI
+
+// call our action
+struct DeviceRotationViewModifier: ViewModifier {
+    let action: (UIDeviceOrientation) -> Void
+
+    func body(content: Content) -> some View {
+        content
+            .onAppear()
+            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
+                action(UIDevice.current.orientation)
+            }
+    }
+}
+
+// A View wrapper to make the modifier easier to use
+extension View {
+    func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
+        self.modifier(DeviceRotationViewModifier(action: action))
+    }
+}
