438718011fbcf357a11908e28ec9307aaa5cd2b1
Kim, Min Ho <xho1995@gmail.com>
2021-05-06 16:04:04 +0900

    adds the test code of the coordinateSpace function in the Apple Developer Documentation

---
+++ CoordinateSpaceTest/CoordinateSpaceTest/CoordinateSpaceTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinateSpaceTestApp.swift
+//  CoordinateSpaceTest
+//
+//  Created by Kim, Min Ho on 2021/05/06.
+//
+
+import SwiftUI
+
+@main
+struct CoordinateSpaceTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
