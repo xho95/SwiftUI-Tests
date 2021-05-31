0f64b1fe867dd1311b98f125cf438d7fd3dcfee4
Kim, Min Ho <xho1995@gmail.com>
2021-05-31 16:13:21 +0900

    initializes the project for UIImageView with MultiTouch

---
+++ ImageViewUI/ImageViewUI/ImageViewUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageViewUIApp.swift
+//  ImageViewUI
+//
+//  Created by Kim, Min Ho on 2021/05/31.
+//
+
+import SwiftUI
+
+@main
+struct ImageViewUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
