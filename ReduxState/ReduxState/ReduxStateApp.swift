04a4658fc3dada970a14205d8d197bd8fdc9740d
Kim, Min Ho <xho1995@gmail.com>
2021-06-03 15:47:38 +0900

    initializes a project for Redux framework with SwiftUI techniques

---
+++ ReduxState/ReduxState/ReduxStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxStateApp.swift
+//  ReduxState
+//
+//  Created by Kim, Min Ho on 2021/06/03.
+//
+
+import SwiftUI
+
+@main
+struct ReduxStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
