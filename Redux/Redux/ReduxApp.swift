3cb68524c382b7c9beb64d75d6beaa6c685c9ac5
Kim, Min Ho <xho1995@gmail.com>
2021-06-03 13:34:38 +0900

    initializes a project for the Redux pattern

---
+++ Redux/Redux/ReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxApp.swift
+//  Redux
+//
+//  Created by Kim, Min Ho on 2021/06/03.
+//
+
+import SwiftUI
+
+@main
+struct ReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
