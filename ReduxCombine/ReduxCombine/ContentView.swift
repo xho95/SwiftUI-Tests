5c76c019eafa35f4123df51586725b0f8f62c85f
Kim, Min Ho <xho1995@gmail.com>
2021-06-03 14:57:53 +0900

    initializes a project for the Redux pattern with Combine framework

---
+++ ReduxCombine/ReduxCombine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReduxCombine
+//
+//  Created by Kim, Min Ho on 2021/06/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
