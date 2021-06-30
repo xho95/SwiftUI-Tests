e0d53c676b89a93d079fa7f1b164fc2a21b622c1
Kim, Min Ho <xho1995@gmail.com>
2021-06-30 15:38:58 +0900

    initializes the project for UnitTest

---
+++ UnitTest/UnitTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnitTest
+//
+//  Created by Kim, Min Ho on 2021/06/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UnitTest/UnitTest/UnitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestApp.swift
+//  UnitTest
+//
+//  Created by Kim, Min Ho on 2021/06/30.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
