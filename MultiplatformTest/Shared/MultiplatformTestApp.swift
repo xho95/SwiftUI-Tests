4c4e182159a9b37518774479ef1501f055e47b84
Kim, Min Ho <xho1995@gmail.com>
2021-06-30 16:06:12 +0900

    renames the project's name with MultiplatformTest

---
+++ MultiplatformTest/Shared/MultiplatformTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplatformTestApp.swift
+//  Shared
+//
+//  Created by Kim, Min Ho on 2021/06/30.
+//
+
+import SwiftUI
+
+@main
+struct MultiplatformTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MultiplatformTest/Shared/PlayData.swift
@@ -0,0 +1,12 @@
+//
+//  PlayData.swift
+//  MultiplatformTest
+//
+//  Created by Kim, Min Ho on 2021/06/30.
+//
+
+import Foundation
+
+class PlayData {
+    var allWords = [String]()
+}
