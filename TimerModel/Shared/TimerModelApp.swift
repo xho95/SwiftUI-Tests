a0b551128d51562369a337aa98711cf58d3444c9
Kim, Min Ho <xho1995@gmail.com>
2021-07-30 16:35:53 +0900

    add a reference link of the project

--- TimerModel/Shared/TimerModelApp.swift
+++ TimerModel/Shared/TimerModelApp.swift
@@ -5,6 +5,8 @@
 //  Created by Kim, Min Ho on 2021/07/30.
 //
 
+// reference : https://stackoverflow.com/questions/57796877/swift-combine-using-timer-publisher-in-an-observable-object
+
 import SwiftUI
 
 @main
