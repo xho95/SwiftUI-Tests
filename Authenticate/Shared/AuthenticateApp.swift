be9cc8c6a133cd81e4b390788ad24ec47e0fbb44
Kim, Min Ho <xho1995@gmail.com>
2021-08-05 15:17:32 +0900

    add an AuthenticateUser project by using GameKit framework

---
+++ Authenticate/Shared/AuthenticateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticateApp.swift
+//  Shared
+//
+//  Created by Kim, Min Ho on 2021/08/05.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
