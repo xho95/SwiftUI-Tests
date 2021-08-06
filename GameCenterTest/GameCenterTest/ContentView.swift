d91849e6ddce2a25f290ff851a808e82a99a58cc
Kim, Min Ho <xho1995@gmail.com>
2021-08-06 15:17:35 +0900

    add a Game Center test app

---
+++ GameCenterTest/GameCenterTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GameCenterTest
+//
+//  Created by Kim, Min Ho on 2021/08/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GameCenterTest/GameCenterTest/GameCenterTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameCenterTestApp.swift
+//  GameCenterTest
+//
+//  Created by Kim, Min Ho on 2021/08/06.
+//
+
+import SwiftUI
+
+@main
+struct GameCenterTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
