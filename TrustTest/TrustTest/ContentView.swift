d23b42dc50128ff6907b50a49975ccf421c6f1f7
Kim, Min Ho <xho1995@gmail.com>
2021-09-24 16:28:32 +0900

    add the TrustTest app

---
+++ TrustTest/TrustTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrustTest
+//
+//  Created by Kim, Min Ho on 2021/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrustTest/TrustTest/TrustTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrustTestApp.swift
+//  TrustTest
+//
+//  Created by Kim, Min Ho on 2021/09/24.
+//
+
+import SwiftUI
+
+@main
+struct TrustTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
