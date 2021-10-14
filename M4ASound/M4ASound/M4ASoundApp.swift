666965660a21c5ef2634480807443986777f3d5a
Kim, Min Ho <xho1995@gmail.com>
2021-10-14 14:18:00 +0900

    add a project to test m4a file play

---
+++ M4ASound/M4ASound/M4ASoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  M4ASoundApp.swift
+//  M4ASound
+//
+//  Created by Kim, Min Ho on 2021/10/14.
+//
+
+import SwiftUI
+
+@main
+struct M4ASoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
