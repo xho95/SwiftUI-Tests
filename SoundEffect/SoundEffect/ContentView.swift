415bd30fa1d1c0ca1e10e48a650b1a8d5e575d2b
Kim, Min Ho <xho1995@gmail.com>
2022-02-06 15:25:41 +0900

    add a sample code for SoundEffect

---
+++ SoundEffect/SoundEffect/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SoundEffect
+//
+//  Created by Kim, Min Ho on 2022/02/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SoundEffect/SoundEffect/SoundEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundEffectApp.swift
+//  SoundEffect
+//
+//  Created by Kim, Min Ho on 2022/02/06.
+//
+
+import SwiftUI
+
+@main
+struct SoundEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
