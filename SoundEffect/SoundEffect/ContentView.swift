b8edb481c2cd583f5eef8d894ccfb751d5049c07
Kim, Min Ho <xho1995@gmail.com>
2022-02-14 15:55:11 +0900

    modify the AVAudioPlayer logic with currentTime

--- SoundEffect/SoundEffect/ContentView.swift
+++ SoundEffect/SoundEffect/ContentView.swift
@@ -6,11 +6,51 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    @State var backgroundOn = true
+    
+    var deleteEffect: AVAudioPlayer?
+    var keyEffect: AVAudioPlayer?
+
+    init() {
+        let keyEffectURL = Bundle.main.url(forResource: "Number Key 01", withExtension: "m4a")!
+        
+        keyEffect = try? AVAudioPlayer(contentsOf: keyEffectURL)
+        keyEffect?.prepareToPlay()
+
+        let deleteEffectURL = Bundle.main.url(forResource: "Delete Key 01", withExtension: "m4a")!
+        
+        deleteEffect = try? AVAudioPlayer(contentsOf: deleteEffectURL)
+        deleteEffect?.prepareToPlay()
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Button {
+                // play an sound effect
+            } label: {
+                Text(backgroundOn ? "Background Off" : "Background On")
+                    .padding()
+            }
+            
+            Button {
+                deleteEffect?.currentTime = 0
+                deleteEffect?.play()
+            } label: {
+                Text("Delete!")
+                    .padding()
+            }
+            
+            Button {
+                keyEffect?.currentTime = 0
+                keyEffect?.play()
+            } label: {
+                Text("Number!")
+                    .padding()
+            }
+        }
     }
 }
 
---
+++ SoundEffect/SoundEffect/SoundEngine.swift
@@ -0,0 +1,48 @@
+//
+//  SoundEngine.swift
+//  SoundEffect
+//
+//  Created by Kim, Min Ho on 2022/02/06.
+//
+
+import Foundation
+import AVKit
+
+struct SoundEngine {
+//    let fileName: String
+    
+    private let engine: AVAudioEngine
+    private let player: AVAudioPlayerNode
+
+    private var audioFile: AVAudioFile
+
+    init(fileName: String = "Number Key 01") {
+//        self.fileName = fileName
+        
+        self.engine = AVAudioEngine()
+        self.player = AVAudioPlayerNode()
+
+        let effectURL = Bundle.main.url(forResource: fileName, withExtension: "m4a")!
+
+        self.audioFile = try! AVAudioFile(forReading: effectURL)
+        
+        self.configureEngine()
+    }
+    
+    func play() {
+        player.scheduleFile(audioFile, at: nil)
+        player.play()
+    }
+
+    private func configureEngine() {
+        engine.attach(player)
+        engine.connect(player, to: engine.outputNode, format: audioFile.processingFormat)
+        engine.prepare()
+        
+        do {
+            try engine.start()
+        } catch {
+            print("Error starting the player: \(error.localizedDescription)")
+        }
+    }
+}
