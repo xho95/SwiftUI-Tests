7eb58b512ff346f883d37a39b4d07ddc0ac0a32a
Kim, Min Ho <xho1995@gmail.com>
2023-03-15 16:26:14 +0900

    Add a CoreLocationTest project to learn how to use a Delegate with SwiftUI

---
+++ CoreLocationTest/CoreLocationTest/CoreLocationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreLocationTestApp.swift
+//  CoreLocationTest
+//
+//  Created by Min Ho Kim on 2023/03/15.
+//
+
+import SwiftUI
+
+@main
+struct CoreLocationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
